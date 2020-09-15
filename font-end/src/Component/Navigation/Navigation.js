aa8c79a12124782915ff7e96753c2de5ef8c2d46
porosh2002 <jamilkashem2002@gmail.com>
2020-09-15 18:06:14 +0600

    a

--- font-end/src/Component/Navigation/Navigation.js
+++ font-end/src/Component/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Navigation() {
     return (
-        <div>
-            <h1>Navigation</h1>
+        <div className="">
+        
         </div>
     )
 }
